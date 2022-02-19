1e95ee955115e1495cd2ce3cfe5b358fe707c2b3
Antonio Dottori <aadottori@gmail.com>
2022-02-19 12:34:32 -0300

    Add createPost method

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -4,7 +4,6 @@ import PostMessage from '../models/postMessage.js';
 export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
-                
         res.status(200).json(postMessages);
     } catch (error) {
         res.status(404).json({ message: error.message});
@@ -12,12 +11,13 @@ export const getPosts = async (req, res) => {
 };
 
 
-export const createPost = (req, res) => {
+export const createPost = async (req, res) => {
     const post = req.body;
     const newPost = new PostMessage(post);
     try {
-
+        await newPost.save();
+        res.status(201).json(newPost);
     } catch (error) {
-
+        res.status(409).json({ message: error.message })
     }
 }
