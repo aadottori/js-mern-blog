55329e6d1280c9c6ce12a8b96d45e5237f647755
Antonio Dottori <aadottori@gmail.com>
2022-02-19 11:41:45 -0300

    Added .gitignore and .env

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import dotenv from 'dotenv';
+dotenv.config({path: "../.env"});
+
 import postRoutes from './routes/posts.js';
 
 const app = express();
@@ -13,8 +16,13 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://mongol:mongol123@cluster0.pcrqq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD= process.env.DB_PASSWORD;
+
+
+const CONNECTION_URL = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.pcrqq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 5000;
+
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
